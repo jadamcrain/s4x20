478d907e1245930a2d952a47d2e8b8163e3a592f
jadamcrain <jadamcrain@gmail.com>
2019-12-28 11:53:53 -0800

    ownership and references

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,17 @@
+
+fn say_hello_by_value(name: String) {
+    println!("Hello {}!", name);
+}
+
+fn say_hello_by_reference(name: &String) {
+    println!("Hello {}!", name);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let name = String::from("Jim");
+    say_hello_by_reference(&name);
+    say_hello_by_value(name);
+
+    // Can't use a value that's already been moved
+    // say_hello_by_value(name);
 }
