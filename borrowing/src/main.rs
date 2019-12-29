bf69f1551b764f4f5e5ba3e66ccdba453bbf0106
jadamcrain <jadamcrain@gmail.com>
2019-12-28 16:45:04 -0800

    starting point for concurrency example

--- borrowing/src/main.rs
+++ borrowing/src/main.rs
@@ -7,16 +7,20 @@ fn select_largest<'a>(a: &'a Value, b: &'a Value) -> &'a Value {
     if a.inner > b.inner { a } else { b }
 }
 
+/* uncomment me
 fn print_by_value(value : Value) {
     println!("value is {}", value.inner);
 }
+*/
 
 fn main() {
     let x = Value { inner: 42 };
     let y = Value { inner: 77 };
 
     let largest = select_largest(&x, &y);
-    // The compiler won't let us 'move' either x or y and then later use the borrowed value
-    // print_by_value(x);
+    /* uncomment me
+       The compiler won't let us 'move' either x or y and then later use the borrowed value
+       print_by_value(x);
+    */
     println!("Largest is {}", largest.inner);
 }
