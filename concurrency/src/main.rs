c3d98d49c73ca571574d3a26fb8bf51343099368
jadamcrain <jadamcrain@gmail.com>
2019-12-28 18:36:12 -0800

    concurrency via counter

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,5 +1,24 @@
-
+use std::sync::{Mutex, Arc};
+use std::thread::JoinHandle;
 
 fn main() {
+    let counter = Arc::new(Mutex::new(0u64));
 
+    let mut handles : Vec<JoinHandle<()>> = Vec::new();
+
+    for _ in 0..10 {
+        let counter = counter.clone();
+        let handle = std::thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("count is {}", *counter.lock().unwrap());
 }
